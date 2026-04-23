830e62c85f5c60019aa646bf1a37eaefbb24bb5b
Sergey Varlamov <timeout2x@gmail.com>
2026-04-23 21:12:49 +0200

    Update gallery-app to 20260423.8 with real download URL

--- Casks/gallery-app.rb
+++ Casks/gallery-app.rb
@@ -1,8 +1,8 @@
 cask "gallery-app" do
-  version "1.0.0"
-  sha256 "PLACEHOLDER_SHA256"
+  version "20260423.8"
+  sha256 "85595903076c55636e64523935bcd98a2726d861663c40d280e9d35f1bdd01a0"
 
-  url "PLACEHOLDER_URL"
+  url "https://iddqd.blob.core.windows.net/gallery-releases/20260423.8/GalleryApp-28ec081.dmg?se=2027-04-23T19%3A11Z&sp=r&sv=2022-11-02&sr=b&sig=mYflkc%2FFijYAvVcl8JnxgAVRDaNfKod9q4A6tGGY2XE%3D"
   name "Gallery App"
   desc "Photo organization app with face clustering, scene detection, and EXIF tagging"
   homepage "https://dev.azure.com/iddqd-hr/claudegallery"
