8ac756fd39f2494fb18e31f75368d3fc4255dc75
Azure Pipeline <pipeline@iddqd.hr>
2026-05-02 09:10:40 +0200

    Update gallery-app to 20260502.1

--- Casks/gallery-app.rb
+++ Casks/gallery-app.rb
@@ -1,8 +1,8 @@
 cask "gallery-app" do
-  version "20260424.4"
-  sha256 "c8d3599b5e7d1d3bb15d410d9e99d2b24e58cbec856e2b33fe47fd0556a6c80d"
+  version "20260502.1"
+  sha256 "a28d9324a8c41e9fb89237547a6a2c7880b612d714360e7c2d7f75de65103732"
 
-  url "https://iddqd.blob.core.windows.net/gallery-releases/20260424.4/GalleryApp-v0.1.1.dmg?se=2027-04-24T19%3A18Z&sp=r&sv=2026-02-06&sr=b&sig=otIQL3d0Y84ZVvY%2F%2F5hJDtvqNRH8s%2FNaG7Mpd%2Frn99E%3D"
+  url "https://iddqd.blob.core.windows.net/gallery-releases/20260502.1/GalleryApp-v0.1.2.dmg?se=2027-05-02T07%3A10Z&sp=r&sv=2026-02-06&sr=b&sig=rzfzlDkSlQ0MW5X6WfaiEk%2Fjeo9Qg2K3mJS%2B0pkGDhE%3D"
   name "Gallery App"
   desc "Photo organization app with face clustering, scene detection, and EXIF tagging"
   homepage "https://dev.azure.com/iddqd-hr/claudegallery"
